360e2c0c754b2f3ec6f1c6309ad9b1e6841a79a5
NicolasFernandoEspitiaCastillo <186082646+NicolasFernandoEspitiaCastillo@users.noreply.github.com>
2024-10-30 13:05:17 -0500

    SolucionPalabraMasLarga

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
-#Escriba un programa que pida dos números enteros y que calcule la división, indicando si la división es exacta o no.
+#Escriba un programa que pida al usuario dos palabras, y que indique cuál de ellas es la más larga y por cuántas letras lo es.
 
-#Dividendo: 14
-#Divisor: 5
-
-#La división no es exacta.
-#Cociente: 2
-#Resto: 4
-#Dividendo: 100
-#Divisor: 10
-
-#La división es exacta.
-#Cociente: 10
-#Resto: 0
+#Palabra 1: edificio
+#Palabra 2: tren
+#La palabra edificio tiene 4 letras mas que tren.
+#Palabra 1: sol
+#Palabra 2: paralelepipedo
+#La palabra paralelepipedo tiene 11 letras mas que sol
+#Palabra 1: plancha
+#Palabra 2: lapices
+#Las dos palabras tienen el mismo largo
 
 
+# Programa para comparar la longitud de dos palabras
 
+# Solicitar al usuario que ingrese dos palabras
+palabra1 = input("Palabra 1: ")
+palabra2 = input("Palabra 2: ")
 
-# Programa para calcular la división y determinar si es exacta
+# Calcular las longitudes de las palabras
+longitud1 = len(palabra1)
+longitud2 = len(palabra2)
 
-# Solicitar al usuario el dividendo y el divisor
-dividendo = int(input("Dividendo: "))
-divisor = int(input("Divisor: "))
-
-# Realizar la división
-cociente = dividendo // divisor
-resto = dividendo % divisor
-
-# Mostrar si la división es exacta o no
-if resto == 0:
-    print("La división es exacta.")
+# Comparar las longitudes y mostrar el resultado
+if longitud1 > longitud2:
+    diferencia = longitud1 - longitud2
+    print(f"La palabra {palabra1} tiene {diferencia} letras más que {palabra2}.")
+elif longitud2 > longitud1:
+    diferencia = longitud2 - longitud1
+    print(f"La palabra {palabra2} tiene {diferencia} letras más que {palabra1}.")
 else:
-    print("La división no es exacta.")
-
-# Mostrar el cociente y el resto
-print(f"Cociente: {cociente}")
-print(f"Resto: {resto}")
-
+    print("Las dos palabras tienen el mismo largo")
 
 
