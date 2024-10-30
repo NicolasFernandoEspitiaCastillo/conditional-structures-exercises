a2cc2cc624336ba651bebf4126a5828abf29f79f
NicolasFernandoEspitiaCastillo <186082646+NicolasFernandoEspitiaCastillo@users.noreply.github.com>
2024-10-30 13:08:47 -0500

    SolucionOrdenamiento

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-#Escriba un programa que pida al usuario dos palabras, y que indique cuál de ellas es la más larga y por cuántas letras lo es.
+#Escriba un programa que reciba como entrada dos números, y los muestre ordenados de menor a mayor:
 
-#Palabra 1: edificio
-#Palabra 2: tren
-#La palabra edificio tiene 4 letras mas que tren.
-#Palabra 1: sol
-#Palabra 2: paralelepipedo
-#La palabra paralelepipedo tiene 11 letras mas que sol
-#Palabra 1: plancha
-#Palabra 2: lapices
-#Las dos palabras tienen el mismo largo
+#Ingrese numero: 51
+#Ingrese numero: 24
+#24 51
+#A continuación, escriba otro programa que haga lo mismo con tres números:
 
+#Ingrese numero: 8
+#Ingrese numero: 1
+#Ingrese numero: 4
+#1 4 8
+#Finalmente, escriba un tercer programa que ordene cuatro números:
 
-# Programa para comparar la longitud de dos palabras
+#Ingrese numero: 7
+#Ingrese numero: 0
+#Ingrese numero: 6
+#Ingrese numero: 1
+#0 1 6 7
+#Recuerde que su programa debe entregar la solución correcta para cualquier combinación de números, no sólo para los ejemplos mostrados aquí.
 
-# Solicitar al usuario que ingrese dos palabras
-palabra1 = input("Palabra 1: ")
-palabra2 = input("Palabra 2: ")
-
-# Calcular las longitudes de las palabras
-longitud1 = len(palabra1)
-longitud2 = len(palabra2)
-
-# Comparar las longitudes y mostrar el resultado
-if longitud1 > longitud2:
-    diferencia = longitud1 - longitud2
-    print(f"La palabra {palabra1} tiene {diferencia} letras más que {palabra2}.")
-elif longitud2 > longitud1:
-    diferencia = longitud2 - longitud1
-    print(f"La palabra {palabra2} tiene {diferencia} letras más que {palabra1}.")
-else:
-    print("Las dos palabras tienen el mismo largo")
+#Hay más de una manera de resolver cada ejercicio.
 
 
+
+# Programa para ordenar cuatro números
+
+# Solicitar al usuario que ingrese cuatro números
+numero1 = int(input("Ingrese numero: "))
+numero2 = int(input("Ingrese numero: "))
+numero3 = int(input("Ingrese numero: "))
+numero4 = int(input("Ingrese numero: "))
+
+# Ordenar y mostrar los números
+numeros = [numero1, numero2, numero3, numero4]
+numeros.sort()
+print(*numeros)  # El asterisco (*) descompone la lista para imprimir los elementos
